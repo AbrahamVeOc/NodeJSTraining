5a785fbce77223e8d9351ddd5fd086c829a473e7
AbrahamVeOc <abrahamveoc@gmail.com>
2022-05-19 15:19:37 -0500

    udemy sintax session

---
+++ Udemy/js/main.js
@@ -0,0 +1,16 @@
+/* Comentarios en bloque
+de esta sección de Sintaxis de JavaScript */
+
+// Comentarios en línea
+
+// Características de la sintaxis JavaScript
+
+// - Ignora los espacios en blanco
+// - Es de tipo case-sensitive (distingue mayúsculas de minúsculas)
+// - Las instrucciones y sentencias finalizan con punto y coma (opcional)
+
+console.log('¡Hola Mundo!');
+
+let objetoEjemplo = {
+    nombre: 'Juan'
+}

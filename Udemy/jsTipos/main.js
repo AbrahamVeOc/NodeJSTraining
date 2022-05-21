b2f6186ed35e7ab15d842a2d1063814451d46d99
AbrahamVeOc <abrahamveoc@gmail.com>
2022-05-20 22:21:02 -0500

    Types Udemy Session

---
+++ Udemy/jsTipos/main.js
@@ -0,0 +1,39 @@
+// Tipos de datos
+
+// Tipado de datos es débil y dinámico
+
+let email = 'pedro@pedro.com';
+email = 20;
+
+// Tipos de datos primitivos
+
+// string o cadena de caracteres (Comillas dobles o simples)
+
+let ciudad = 'Villa de \nMadrid';
+let calle = 'Avenida O\'Donell';
+
+console.log(ciudad, calle);
+
+// number números en todos los tipos disponibles: enteros, flotantes, exponenciales, etc...
+
+let resultado = 8.6;
+
+// boolean (true o false)
+
+let mayorEdad = false;
+
+// undefined (tipo-valor)
+
+let marca; // valor undefined cuando no ha sido aún asignada
+
+console.log(marca);
+
+// null (tipo-valor)
+
+let localidad = null;
+
+// Tipos compuestos
+
+// Objetos
+
+console.log(typeof mayorEdad);

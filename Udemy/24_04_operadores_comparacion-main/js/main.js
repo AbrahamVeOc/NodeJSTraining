450e575cdd1513beb371f7e9fef6a6c0bc00414d
AbrahamVeOc <abrahamveoc@gmail.com>
2022-05-21 15:08:33 -0500

    Day 6: Comparison Operators Udemy Session

---
+++ Udemy/24_04_operadores_comparacion-main/js/main.js
@@ -0,0 +1,57 @@
+// Operadores de asignación
+
+// Asignación =
+
+// Asignación de adición +=
+
+let puntuacion1 = 4;
+let puntuacion2 = 9;
+
+puntuacion1 += puntuacion2;
+console.log(puntuacion1);
+
+// -= *= /= %=
+
+// Operadores de comparación
+
+// Operador de igualdad ==
+
+let ciudad = 'Madrid';
+let termino = 'Madrid';
+
+let hayCoincidencia = ciudad == termino;
+console.log(hayCoincidencia);
+
+// No es estricto con los tipos de datos
+
+let valor1 = 3;
+let valor2 = '3';
+
+hayCoincidencia = valor1 == valor2;
+console.log(hayCoincidencia);
+
+// Operador de igualdad estricta ===
+
+hayCoincidencia = valor1 === valor2;
+console.log(hayCoincidencia);
+
+// Operador de desigualdad !=
+
+// Operador de desigualdad estricta !==
+
+hayCoincidencia = valor1 !== valor2;
+console.log(hayCoincidencia);
+
+// Operador mayor que >
+
+valor1 = 8.4;
+valor2 = 3;
+
+resultado = valor1 > valor2;
+console.log(resultado);
+
+// Operador menor que <
+
+// Operador mayor o igual que >=
+
+// Operador menor o igual que <=

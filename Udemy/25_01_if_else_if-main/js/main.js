56ae4deacb383afd1b8b656fc814d660e3cbd755
AbrahamVeOc <abrahamveoc@gmail.com>
2022-05-22 16:38:31 -0500

    Day 7: if Udemmy Session commentary correction

--- Udemy/25_01_if_else_if-main/js/main.js
+++ Udemy/25_01_if_else_if-main/js/main.js
@@ -1,3 +1,5 @@
+// Estructura condicional if
+
 let nombre = "Lucía";
 let edad = 15;
 let mayorEdad;
@@ -7,6 +9,11 @@ if (edad >= 18) {
   console.log(nombre + " es mayor de edad");
 }
 
+// if (edad < 18)
+//     console.log('Menor edad'); No se suele emplear el alivio de llaves
+
+// Estructura if else
+
 let a = 10;
 let b = 20;
 
@@ -15,6 +22,8 @@ if (a > b) {
 } else {
   console.log("a es menor o igual que b");
 }
+
+// Estructura if else if
 
 a = b;
 

655ea04294d2121d9723c4d8bfa21548a6662acc
AbrahamVeOc <abrahamveoc@gmail.com>
2022-05-23 16:42:02 -0500

    Day 8: function introduction Udemy Session

---
+++ Udemy/26_01_funciones-main/js/main.js
@@ -0,0 +1,18 @@
+// Sintaxis de declaración de funciones
+
+function suma (a, b) {
+    console.log(a + b);
+}
+
+function division (a, b) {
+    let c = a / b;
+    return c;
+    console.log('Hola tras return'); // No se ejecutará
+}
+
+// Sintaxis de invocación
+
+suma(4, 6);
+
+let resultado = division(10,2);
+console.log(resultado);

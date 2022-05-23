d3706d0da7e389be9268cdab22bbbddbd5ac50e7
AbrahamVeOc <abrahamveoc@gmail.com>
2022-05-23 15:39:37 -0500

    Day 8: while vs do while Udemy Session

---
+++ Udemy/25_04_while-main/js/main.js
@@ -0,0 +1,13 @@
+// Estructura iterativa while
+
+let password = '1234';
+
+// while (password !== '1234') {
+//     password = prompt('Escriba su contraseña');
+// }
+
+// Estructura iterativa do while
+
+do {
+    password = prompt('Escriba su contraseña');
+} while (password !== '1234')

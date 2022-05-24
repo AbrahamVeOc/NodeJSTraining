701214ece093f85a009558749eadbfb4d2dd4524
AbrahamVeOc <abrahamveoc@gmail.com>
2022-05-24 17:44:04 -0500

    Day 9: function expresion Udemy Sesion

---
+++ Udemy/26_02_funciones_expresion-main/js/main.js
@@ -0,0 +1,21 @@
+// Expression functions (anónimas)
+
+let cuadrado = function (a) {
+  return a * a;
+};
+
+console.log(cuadrado(5));
+
+// Funciones flecha (ECMAScript 6 2015)
+
+let cubo = (a) => {
+  return a * a * a;
+};
+
+let cubo = (a) => a * a * a;
+
+console.log(cubo(5));
+
+let mensaje = () => console.log("Hola amigos");
+
+mensaje();

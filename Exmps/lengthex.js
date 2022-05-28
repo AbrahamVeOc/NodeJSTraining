47efa93f50bcb785a3ee4fa7b1e62633a6b5989e
AbrahamVeOc <abrahamveoc@gmail.com>
2022-05-27 23:16:06 -0500

    Day 12: length exercise

---
+++ Exmps/lengthex.js
@@ -0,0 +1,6 @@
+let fiarray = [0, 1, 1, 2, 3, 5, 8, 13];
+console.log(`lengthfiarray: `, fiarray.length);
+for (let i = 0; i < fiarray.length; i++) {
+  const element = fiarray[i];
+  console.log(element);
+}

62c9c50f79ebb4e1e626e3dc4a856d8c36783728
AbrahamVeOc <abrahamveoc@gmail.com>
2022-05-29 23:26:41 -0500

    Day 14: currency converter

---
+++ Exmps/currency.js
@@ -0,0 +1,9 @@
+function convert(amount, rate) {
+  return amount * rate;
+}
+
+console.log(convert(10, 1.1));
+console.log(convert(250, 0.04));
+document.write(convert(250, 0.04));
+
+//document.write doesnt works on nodejs bcs its part of the browser ecosistem

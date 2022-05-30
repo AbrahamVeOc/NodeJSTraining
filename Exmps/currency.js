a19535f5d2f753ee2e2c891abcbdac53cde3b7c9
AbrahamVeOc <abrahamveoc@gmail.com>
2022-05-29 23:30:39 -0500

    Day 14: xd comment

--- Exmps/currency.js
+++ Exmps/currency.js
@@ -6,4 +6,4 @@ console.log(convert(10, 1.1));
 console.log(convert(250, 0.04));
 document.write(convert(250, 0.04));
 
-//document.write doesnt works on nodejs bcs its part of the browser ecosistem
+//document.write doesnt works on nodejs bcs its part of the browser ecosistem xd

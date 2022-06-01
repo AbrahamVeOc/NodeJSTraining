345533d1b0be29cd294162530a27eb891d56b595
AbrahamVeOc <abrahamveoc@gmail.com>
2022-05-31 23:58:13 -0500

    Day 16: classes

---
+++ Exmps/Classes.js
@@ -0,0 +1,64 @@
+class Automovil {
+  constructor(gasolina, color, modelo, position = { x: 0 }, estado = "off") {
+    this.gasolina = gasolina;
+    this.color = color;
+    this.modelo = modelo;
+    this.position = position;
+    this.estado = estado;
+  }
+
+  arrancar = function () {
+    if (this.estado === "on") return "calmate perro";
+    if (this.gasolina <= 0) {
+      this.gasolina = 0;
+      return "out of gas";
+    }
+    console.log("arranco", this.modelo);
+    this.estado = "on";
+    this.gasolina = this.gasolina - 3;
+    return `arranco exitosamente y quedan ${this.gasolina} litros de gasolina`;
+  };
+
+  apagar = function () {
+    this.estado = "off";
+  };
+
+  avanzar = function (mt) {
+    if (this.estado !== "on") return "off state";
+    if (mt * 0.2 > this.gasolina) return "Dont enough gas";
+    this.position = { x: this.position.x + mt };
+    this.gasolina = this.gasolina - 0.2 * mt;
+    return this.position;
+  };
+}
+
+// const bmw = new Automovil(10, "red", "E230", { x: 40 }, "on");
+// console.log(bmw.arrancar());
+
+const mb = new Automovil(20, "grey", "EC20");
+console.log(mb.arrancar());
+mb.apagar();
+mb.arrancar();
+mb.apagar();
+mb.arrancar();
+console.log(mb.avanzar(1100));
+console.log(mb.avanzar(11));
+
+// const viejo = new Automovil(0, "black", "viejoCR7");
+// console.log(viejo.arrancar());
+
+class Animal {
+  constructor(classe) {
+    this.classe;
+  }
+  caminar = function () {
+    console.log("camine");
+  };
+}
+
+class Dog extends Animal {
+  caminar() {
+    super.caminar();
+    console.log("lorem");
+  }
+}

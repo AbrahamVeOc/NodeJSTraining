0922443d6788d40c3e6c9817b597e2fd8e9a40cd
rojo <rojo_placebo@hotmail.com>
2022-06-02 14:09:17 -0500

    add race Class and recursive method

--- CarRace/race.js
+++ CarRace/race.js
@@ -1,9 +1,21 @@
+const { Car } = require("./car");
+
 class Race {
-  constructor(cars, length = 100) {
+  constructor(cars, length = 100, finalizo = false) {
     this.cars = cars;
     this.length = length;
   }
-  // rSG () {
-  //   if 
-  // }
+  rSG() {
+    for (let index = 0; index < 100; index++) {
+      console.log(this.cars[0].run());
+      console.log(this.cars[1].run());
+      if (index === 9) this.finalizo = true;
+    }
+    if ((this.finalizo = false)) return this.rSG();
+  }
 }
+
+const car1 = new Car("Rojinator 5000", "blue");
+const car2 = new Car("Tinkinator 12000", "purple");
+const race = new Race([car1, car2]);
+race.rSG();
